1ce5a1cd54171f1f127cd2e066e6973a3c56fa21
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-02-18 10:36:40 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
+#require "pry"
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,12 +15,75 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    
+
+    #elsif if match cart
+    #
+    elsif req.path.match(/cart/)
+      if @@cart.length >= 1
+        @@cart.each do |cart|
+          resp.write "#{cart}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      #binding.pry
+      if @@items.include?(item)
+        @@cart << item
+       
+         resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
+
+
+
+
+
+    #elsif req.path.match(/search/)
+     # search_term= req.params["q"]
+
+      #if @@items.include?(search_term)
+       # @@cart << search_term
+     # end
+   # elsif req.path.match(/add/)
+   # @@items.each do |item| @@cart << item
+   #end
     else
       resp.write "Path Not Found"
+     # if req.path.match(/search/)
+       # search_term= req.params["q"]
+        #if @@items.include?(search_term)
+         # @@cart
+       # else @@items.include?(search_term) == false
+        #  resp.write "We don't have that item"
+       # end
+      #end
     end
+
+
+
+  #elsif req.path.match(/search/)
+ 
+    #search_term = req.params["q"]
+
+   # if @@items.include?(search_term)
+    #  resp.write "#{search_term} is one of our items"
+    #else
+   #   resp.write "Couldn't find #{search_term}"
+   # end
+
+    
+
+
 
     resp.finish
   end
+
 
   def handle_search(search_term)
     if @@items.include?(search_term)
@@ -27,4 +92,6 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+
 end
